bd9ee323328c264c71c43b864f77b8198ee5affa
syogo-user <s.photo.trumpet.005@gmail.com>
2022-05-04 15:42:50 +0900

    Jsonを取得するクラスを作成

---
+++ app/src/main/java/com/example/myjettrivia/network/QuestionApi.kt
@@ -0,0 +1,11 @@
+package com.example.myjettrivia.network
+
+import com.example.myjettrivia.model.Question
+import retrofit2.http.GET
+import javax.inject.Singleton
+
+@Singleton
+interface QuestionApi {
+    @GET("world.json")
+    suspend fun getAllQuestions(): Question
+}
